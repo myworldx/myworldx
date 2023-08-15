a4435e8a1b98c568a351d7251df7d5fd347559f2
Rafael <rafinha_fern@hotmail.com>
2023-08-14 21:28:46 -0300

    feat([user]): add new styles to the main page, new globals markdown styles

--- apps/web/src/app/[user]/_page/index.tsx
+++ apps/web/src/app/[user]/_page/index.tsx
@@ -1,10 +1,17 @@
-import { M_PLUS_1 } from 'next/font/google'
-import { UserProfileImageHeader } from './_components/userProfileImageHeader'
-import App from '@/lib/octokit'
-import { parse } from 'node-html-parser'
+import { M_PLUS_1, Roboto } from 'next/font/google'
 
-const logoFont = M_PLUS_1({
-  weight: '800',
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { parse } from 'node-html-parser'
+import App from '@/lib/octokit'
+
+import LogoExtended from '@/public/myworldx_logo_ext.png'
+
+import { UserProfileImageHeader } from './_components/userProfileImageHeader'
+
+const bodyFont = Roboto({
+  weight: ['400', '700'],
   subsets: ['latin'],
 })
 
@@ -13,7 +20,7 @@ export default async function UserPage({ params }: { params: { user: string } })
 
   const RawMarkdown = await fetch('https://raw.githubusercontent.com/raferdev/raferdev/main/README.md')
 
-  const octokit = await App.getInstallationOctokit(40324534)
+  const octokit = await App.getInstallationOctokit(40479142)
 
   const { data: HtmlMarkdown } = await octokit.request('POST /markdown', {
     text: await RawMarkdown.text(),
@@ -52,24 +59,66 @@ export default async function UserPage({ params }: { params: { user: string } })
 
   const __html = rawParsedHtmlMarkdownInnerHtml.replaceAll(/src=(["'])((?:\\\1|(?:(?!\1)).)*)(\1)/g, replacer)
 
+  const MainMenu = () => {
+    const array = ['Home', 'Blog', 'About', 'Contact']
+    return (
+      <nav className="flex h-8 w-full items-center justify-center p-8 text-xl font-normal">
+        <ul className="flex h-full w-full items-center justify-between gap-3">
+          {array.map((item) => (
+            <li className="max-ful">
+              <Link href={`/${item}`}>
+                <span>{item}</span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    )
+  }
+
   return (
-    <div className="flex h-screen w-screen flex-col pt-[150px]">
-      <header className="fixed right-0 top-0 flex h-[150px] w-full items-center justify-center shadow-sm	">
+    <>
+      <header className="flex h-[170px] w-full items-center justify-between">
         <div className="flex items-center justify-center">
-          <div className="relative z-10 mr-[-20px] flex h-[120px] w-[120px] items-center justify-center rounded-full border-[10px] border-gray-950">
-            <div className="absolute left-0 top-0 -z-0 box-content"></div>
-            <UserProfileImageHeader />
+          <div className="relative z-10 flex h-[100px] w-[100px] items-center justify-center rounded-full border-[3px] border-gray-950">
+            <div className="absolute left-0 top-0 -z-0 box-content">
+              <UserProfileImageHeader />
+            </div>
           </div>
-          <h1
-            className={`${logoFont.className} bg-gradient-to-r from-neutral-950 to-slate-900 bg-clip-text p-1 text-[80px] tracking-[-0.1em] text-transparent`}
-          >
-            Raferdev
-          </h1>
+        </div>
+        <div className="flex">
+          <Image height={70} width={120} src={LogoExtended} alt="" />
+        </div>
+        <div className="flex h-[100px] w-[100px] items-center justify-center rounded-full">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-[40px] w-[40px]" viewBox="0 0 512 512">
+            <path
+              d="M192 176v-40a40 40 0 0140-40h160a40 40 0 0140 40v240a40 40 0 01-40 40H240c-22.09 0-48-17.91-48-40v-40"
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="32"
+            />
+            <path
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="32"
+              d="M288 336l80-80-80-80M80 256h272"
+            />
+          </svg>
         </div>
       </header>
-      <div className="h-full w-full flex items-center flex-col p-8">
-        <article className="flex items-start flex-col w-full" dangerouslySetInnerHTML={{ __html }}></article>
+
+      <MainMenu />
+
+      <div className="flex h-full w-full max-w-4xl flex-col items-center">
+        <article
+          className={`${bodyFont.className} flex w-full flex-col items-start`}
+          dangerouslySetInnerHTML={{ __html }}
+        ></article>
       </div>
-    </div>
+    </>
   )
 }
