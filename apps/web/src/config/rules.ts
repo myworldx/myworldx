ff1636a608a44e01a55b3976486c8e5e8ee8ad73
Rafael <rafinha_fern@hotmail.com>
2023-08-22 20:44:51 -0300

    feat(web/utils): add class name merger on utils
    - Preparing to create components

--- apps/web/src/config/rules.ts
+++ apps/web/src/config/rules.ts
@@ -1,6 +1,17 @@
 const __VALID_EXTENSIONS_MAP = new Map()
 
-__VALID_EXTENSIONS_MAP.set('project', {})
+__VALID_EXTENSIONS_MAP.set('project', {
+  valid: true,
+  name: 'Project',
+  description: 'User can show your projects like a portfolio',
+  icon: 'project',
+  color: 'blue',
+  path: 'project',
+  extensions: ['project'],
+  extensionsKeys: ['project'],
+  extensionsValues: ['project'],
+  extensionsMap: new Map([['project', 'project']]),
+})
 __VALID_EXTENSIONS_MAP.set('index', {})
 __VALID_EXTENSIONS_MAP.set('blog', {})
 __VALID_EXTENSIONS_MAP.set('post', {})
