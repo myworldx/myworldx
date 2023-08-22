1d38a484bb3c63eae818e98f1c4bb1208cb8a8bf
Rafael <rafinha_fern@hotmail.com>
2023-08-22 05:57:51 -0300

    feat(web/@types): add new shortest octokit webhooks types
    - Delete webhooks file types - Import all from @octokit/webhooks

--- apps/web/src/@types/octokit/core.d.ts
+++
@@ -1,8 +0,0 @@
-import { EmitterWebhookEventName } from '@octokit/webhooks'
-import { InstallationEvent } from '../webhooks/schemas'
-
-export type WebhooksOnEventCallback = {
-  id: string
-  name: EmitterWebhookEventName
-  payload: InstallationEvent
-}
